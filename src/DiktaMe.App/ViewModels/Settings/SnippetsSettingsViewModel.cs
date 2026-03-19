71182d7422d1b7661d85aeb920f4cafa96f83789
Eduardo García-Torres Resano <geckogt@gmail.com>
2026-03-19 16:10:08 -0600

    style: sort using directives to fix dotnet format CI failure [I18N]

--- src/DiktaMe.App/ViewModels/Settings/SnippetsSettingsViewModel.cs
+++ src/DiktaMe.App/ViewModels/Settings/SnippetsSettingsViewModel.cs
@@ -2,8 +2,8 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using DiktaMe.Core.Config;
 using DiktaMe.App.Services;
+using DiktaMe.Core.Config;
 using Serilog;
 
 namespace DiktaMe.App.ViewModels.Settings;
